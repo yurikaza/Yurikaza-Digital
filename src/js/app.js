2d1101e6cc24abd157c96ea55ebf6a3b54fc2ca0
yurikaza <yusufmirza55@hotmail.com>
2021-10-04 09:30:51 +0300

    a

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
+//import LocomotiveScroll from 'locomotive-scroll';
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true
 });
-
 
 
 /*
